adec282fc5a1a32f7b008d372adcc0eb74c606bb
herrmeik <mikehirt@gmx.ch>
2018-12-20 18:41:19 +0100

    Add router

--- hani-website/src/App.js
+++ hani-website/src/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+
 import './App.css';
-import ProfilePicture from '../src/Picure/ProfilePicture'
+import ProfilePicture from './Picure/ProfilePicture'
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+    ...
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+    ...
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <ProfilePicture></ProfilePicture>
+          <div>
+        <Router>
+    <div>
+      <aside>
+        <Link to={`/`}>Dashboard</Link>
+        <Link to={`/about`}>About</Link>
+      </aside>
+
+      <main>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/about" component={About} />
+      </main>
+    </div>
+  </Router>,
+      </div>
           {/* <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
